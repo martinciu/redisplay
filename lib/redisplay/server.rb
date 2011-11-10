75c1449787b7dd64348f27e8b60c107b79b119db
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2011-11-10 22:25:56 +0100

    fetching different types

--- lib/redisplay/server.rb
+++ lib/redisplay/server.rb
@@ -22,15 +22,40 @@ module Redisplay
         content_type :json
         body.to_json
       end
+
+      def redis
+        @redis ||= Redisplay.redis
+      end
+
+      def value(key)
+        case redis.type(key)
+          when "string" then redis.get(params[:key])
+          when "hash" then redis.hgetall(params[:key])
+          when "list" then
+            len = redis.llen(params[:key])
+            list = []
+            for i in 0..len - 1
+              list << redis.lindex(params[:key], i)
+            end
+            list
+          when "set" then redis.smembers(params[:key])
+          when "zset" then redis.zrange(params[:key], 0, -1)
+          else ""
+        end
+      end
   
-  end
+    end
 
     get '/' do
       erb :index
     end
 
     get '/keys/:pattern' do
-      render_json Redisplay.redis.keys(params[:pattern]).sort.first(100)
+      render_json redis.keys(params[:pattern]).sort.first(100)
+    end
+
+    get '/key/:key' do
+      render_json value(params[:key])
     end
 
   end
--- spec/redisplay/server_spec.rb
+++ spec/redisplay/server_spec.rb
@@ -12,7 +12,13 @@ describe Redisplay::Server do
     Redisplay.redis.set('mystring', 'string')
     Redisplay.redis.set('hisstring', 'hisstring')
     Redisplay.redis.hset('myhash', "foo", "1")
-    Redisplay.redis.hset('myhash', "bar", "1")
+    Redisplay.redis.hset('myhash', "bar", "2")
+    Redisplay.redis.lpush('list', "foo")
+    Redisplay.redis.lpush('list', "bar")
+    Redisplay.redis.sadd('set', "foo")
+    Redisplay.redis.sadd('set', "bar")
+    Redisplay.redis.zadd('zset', 2, "foo")
+    Redisplay.redis.zadd('zset', 1, "bar")
   end
 
   it "should respond to /" do
@@ -27,4 +33,40 @@ describe Redisplay::Server do
     last_response.body.must_equal "[\"myhash\",\"mystring\"]"
   end
 
+  describe "key" do
+    it "/key/mystring returns string" do
+      get '/key/mystring'
+      last_response.status.must_equal 200
+      last_response['Content-Type'].must_equal 'application/json;charset=utf-8'
+      last_response.body.must_equal "\"string\""
+    end
+
+    it "/key/myhash returns hash" do
+      get '/key/myhash'
+      last_response.status.must_equal 200
+      last_response['Content-Type'].must_equal 'application/json;charset=utf-8'
+      last_response.body.must_equal "{\"foo\":\"1\",\"bar\":\"2\"}"
+    end
+
+    it "/key/list returns list" do
+      get '/key/list'
+      last_response.status.must_equal 200
+      last_response['Content-Type'].must_equal 'application/json;charset=utf-8'
+      last_response.body.must_equal "[\"bar\",\"foo\"]"
+    end
+
+    it "/key/set returns set" do
+      get '/key/set'
+      last_response.status.must_equal 200
+      last_response['Content-Type'].must_equal 'application/json;charset=utf-8'
+      last_response.body.must_equal "[\"foo\",\"bar\"]"
+    end
+
+    it "/key/zset returns sortedset" do
+      get '/key/zset'
+      last_response.status.must_equal 200
+      last_response['Content-Type'].must_equal 'application/json;charset=utf-8'
+      last_response.body.must_equal "[\"bar\",\"foo\"]"
+    end
+  end
 end
